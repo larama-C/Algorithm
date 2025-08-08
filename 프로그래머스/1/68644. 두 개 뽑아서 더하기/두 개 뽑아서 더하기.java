fea5e94a910186e786878e9fa6aff25a73b5a3ff
BYEONGJUNE CHOI <56674013+larama-C@users.noreply.github.com>
2025-08-08 15:08:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 7.98 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length; i ++)
+        {
+            for(int j = i + 1; j < numbers.length; j++)
+                if(!list.contains(numbers[i] + numbers[j]))
+                {
+                    list.add(numbers[i] + numbers[j]);
+                }
+        }
+        
+        list.sort(Comparator.naturalOrder());
+
+        return list.stream()
+                   .mapToInt(Integer::intValue)
+                   .toArray();
+    }
+}
