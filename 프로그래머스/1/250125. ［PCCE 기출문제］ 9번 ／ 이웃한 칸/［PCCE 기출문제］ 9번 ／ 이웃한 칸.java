30b9853abc11aef28ec7f952dd43c2391abcd130
BYEONGJUNE CHOI <56674013+larama-C@users.noreply.github.com>
2025-08-15 19:51:27 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        String color = board[h][w];
+        
+        if(h + 1 < board.length && color.equals(board[h+1][w]))
+        {
+            answer++;
+        }
+        if(h - 1 >= 0 && color.equals(board[h-1][w]))
+        {
+            answer++;
+        }
+        if(w + 1 < board.length && color.equals(board[h][w+1]))
+        {
+            answer++;
+        }
+        if(w - 1 >= 0 && color.equals(board[h][w-1]))
+        {
+            answer++;
+        }
+        
+        
+        return answer;
+    }
+}
