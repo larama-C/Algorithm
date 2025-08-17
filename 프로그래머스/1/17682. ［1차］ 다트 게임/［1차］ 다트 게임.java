b82080f4477200d87746b44617a158e9bdf90a6c
BYEONGJUNE CHOI <56674013+larama-C@users.noreply.github.com>
2025-08-17 21:28:44 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.14 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+        int[] score = new int[3];
+        int idx = -1;
+        
+        for(int i = 0; i < dartResult.length(); i++)
+        {
+            char c = dartResult.charAt(i);
+            
+            if(Character.isDigit(c))
+            {
+                idx++;
+                if(c == '1' && i + 1 < dartResult.length() && dartResult.charAt(i + 1) == '0')
+                {
+                    score[idx] = 10;
+                    i++;
+                }
+                else
+                {
+                    score[idx] = c - '0';
+                }
+            }
+            else
+            {
+                if(c == 'D')
+                {
+                    score[idx] = (int)Math.pow(score[idx], 2);
+                }
+                else if(c == 'T')
+                {
+                    score[idx] = (int)Math.pow(score[idx], 3);
+                }
+                else if(c == '*')
+                {
+                    score[idx] *= 2;
+                    if(idx > 0)
+                    {
+                        score[idx - 1] *= 2;
+                    }
+                }
+                else if(c == '#')
+                {
+                    score[idx] *= -1;
+                }
+            }
+        }
+        
+        return Arrays.stream(score).sum();
+    }
+}
