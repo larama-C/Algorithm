5cf5838f06a037dfd42ae0120f9921ad05408d44
BYEONGJUNE CHOI <56674013+larama-C@users.noreply.github.com>
2025-08-30 18:06:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean isStart = true;
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char ch = s.charAt(i);
+
+            if (ch == ' ') 
+            {
+                sb.append(ch);
+                isStart = true;
+            } 
+            else 
+            {
+                if (isStart) 
+                {
+                    sb.append(Character.toUpperCase(ch));
+                    isStart = false;
+                } else {
+                    sb.append(Character.toLowerCase(ch));
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
