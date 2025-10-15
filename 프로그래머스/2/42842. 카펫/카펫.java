b0e52b8e7f4210ce2f83974875363b1d505a1cbb
BYEONGJUNE CHOI <56674013+larama-C@users.noreply.github.com>
2025-10-15 21:52:34 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        
         int sum = brown + yellow;
-        int width = 0;
-        int height = 0;
-        
-        for(height = 1; height <= Math.sqrt(sum); height++)
-        {
-            if(sum % height == 0)
-            {
-                width = sum / height;
-                
-                if((width - 2) * (height - 2) == yellow)
-                {
+
+        for (int height = 1; height <= Math.sqrt(sum); height++) {
+            if (sum % height == 0) {
+                int width = sum / height;
+
+                if ((width - 2) * (height - 2) == yellow) {
                     return new int[] {width, height};
                 }
             }
         }
-        
-        return new int[]{};
+
+        return new int[] {};
     }
 }
